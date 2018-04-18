15e2dbdc2acdb026bbeeedd18f547d6264616adc
Robert M. Ryan <robert.ryan@mindspring.com>
2018-04-18 05:13:06 -0700

    Initial Commit

---
+++ CircleMaskDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CircleMaskDemo
+//
+//  Created by Robert Ryan on 4/18/18.
+//  Copyright © 2018 Robert Ryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
